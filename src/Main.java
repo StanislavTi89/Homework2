d9443b8790b0aa5dc256aa159c5afc96fc02a18a
StanislavTi89 <Timoshenko311989@gmail.com>
2022-11-05 10:22:59 +0300

    Cделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("я_вас_категорически_приветствую_2!");
     }
 }
